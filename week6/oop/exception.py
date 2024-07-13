08f8681df550aaa3253e5cbc48acf7e422d6fa7a
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-07-13 19:04:38 +0300

    added exception.py

---
+++ week6/oop/exception.py
@@ -0,0 +1,14 @@
+# def calc(x,y):
+#     if y == 0:
+#         raise ZeroDivisionError("division by zero is not allowed")
+#     return x/y
+    
+# print(calc(2,0))
+
+def div(x,y):
+    if y == 0:
+        raise ZeroDivisionError ("division by zero is not allowed")
+    return x/y 
+
+print(div(3,3))
+
